06511539102561dc5cec2a55191c71ff2077bea6
Antoan Metodiev <Boksnem@abv.bg>
2024-01-11 23:09:18 +0200

    add task

---
+++ Encapsulation/Encapsulation - Lab/SortByNameAndAge/Main.java
@@ -0,0 +1,37 @@
+package SortByNameAndAge;
+
+import SortByNameAndAge.Person;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<Person> people = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().split("\\s+");
+            Person person = new Person(input[0], input[1], Integer.parseInt(input[2]));
+            people.add(person);
+        }
+
+        Collections.sort(people, (firstPerson, secondPerson) -> {
+            // В int-a връщаше 0, ако са равни!
+            int sComp = firstPerson.getFirstName().compareTo(secondPerson.getFirstName());
+
+            if (sComp != 0) {
+                return sComp;
+            } else {
+                return Integer.compare(firstPerson.getAge(), secondPerson.getAge());
+            }
+        });
+
+        people.forEach(e-> System.out.println(e.toString()));
+
+
+//                .sorted(e -> )
+
+
+    }
+}
