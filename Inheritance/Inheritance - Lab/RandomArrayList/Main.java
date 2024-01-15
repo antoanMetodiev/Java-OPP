daead688d3f2038577bfa3791b1dda9cd9faa9b0
Antoan Metodiev <Boksnem@abv.bg>
2024-01-15 20:00:38 +0200

    add solution

---
+++ Inheritance/Inheritance - Lab/RandomArrayList/Main.java
@@ -0,0 +1,8 @@
+package RandomArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        RandomArrayList arrayList = new RandomArrayList();
+        arrayList.remove(arrayList.getRandomElement());
+    }
+}
