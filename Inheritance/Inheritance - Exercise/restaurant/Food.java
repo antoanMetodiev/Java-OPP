e0dbbdf2a1162a77099b8873af28bd414030f0d0
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-01-18 18:43:34 +0200

    Update Food.java

--- Inheritance/Inheritance - Exercise/restaurant/Food.java
+++ Inheritance/Inheritance - Exercise/restaurant/Food.java
@@ -3,13 +3,6 @@ package restaurant;
 import java.math.BigDecimal;
 
 public class Food extends Product {
-    // Класът Food трябва да има следните елементи:
-    //• Конструктор със следните параметри: String name, BigDecimal price, double grams
-    //• name – String
-    //• price – double
-    //• grams - double
-    //• Getter метод за grams
-
     private double grams;
 
     public Food(String name, BigDecimal price, double grams) {
