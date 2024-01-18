81024f00ab14c0de86c6a63e04f5869c0a91b703
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-01-18 18:42:52 +0200

    Update Coffee.java

--- Inheritance/Inheritance - Exercise/restaurant/Coffee.java
+++ Inheritance/Inheritance - Exercise/restaurant/Coffee.java
@@ -3,12 +3,6 @@ package restaurant;
 import java.math.BigDecimal;
 
 public class Coffee extends HotBeverage {
-    //  Класът Coffee трябва да има следните допълнителни елементи:
-    //• double COFFEE_MILLILITERS = 50
-    //• BigDecimal COFFEE_PRICE = 3.50
-    //• caffeine – double
-    //• Getter метод за caffeine
-
     private static final double COFFEE_MILLILITERS = 50;
     private static final BigDecimal COFFEE_PRICE = new BigDecimal(3.50);
     private double caffeine;
