5132e8e6c4e6d233544f867a39124634bb427ea2
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-01-19 18:55:17 +0200

    Update Car.java

--- Interfaces and Abstraction/Interfaces and Abstraction - Lab/CarShop/Car.java
+++ Interfaces and Abstraction/Interfaces and Abstraction - Lab/CarShop/Car.java
@@ -4,13 +4,9 @@ import java.io.Serializable;
 
 public interface Car extends Serializable {
     int TIRES = 4;
-
+    
     String getModel();
-
     String getColor();
-
     Integer getHorsePower();
-
     String countryProduced();
-
 }
