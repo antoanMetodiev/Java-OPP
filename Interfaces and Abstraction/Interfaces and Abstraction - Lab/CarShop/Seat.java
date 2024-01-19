f609d91929a61c7aa1691563ebd19b162deed851
Antoan Metodiev <Boksnem@abv.bg>
2024-01-19 18:54:37 +0200

    add solution

---
+++ Interfaces and Abstraction/Interfaces and Abstraction - Lab/CarShop/Seat.java
@@ -0,0 +1,40 @@
+package CarShop;
+
+public class Seat implements Car {
+    private String model;
+    private String color;
+    private Integer horsePower;
+    private String country;
+
+    public Seat(String model, String color, Integer horsePower, String country) {
+        this.model = model;
+        this.color = color;
+        this.horsePower = horsePower;
+        this.country = country;
+    }
+
+    @Override
+    public String getModel() {
+        return this.model;
+    }
+
+    @Override
+    public String getColor() {
+        return this.color;
+    }
+
+    @Override
+    public Integer getHorsePower() {
+        return this.horsePower;
+    }
+
+    @Override
+    public String countryProduced() {
+        return this.country;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("This is %s produced in %s and have 4 tires", this.model, this.country);
+    }
+}
