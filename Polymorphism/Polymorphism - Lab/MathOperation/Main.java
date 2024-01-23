556d52b126e64089134dbed87d27125e37e9117d
Antoan Metodiev <Boksnem@abv.bg>
2024-01-23 21:27:23 +0200

    add solution

---
+++ Polymorphism/Polymorphism - Lab/MathOperation/Main.java
@@ -0,0 +1,14 @@
+package MathOperation;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(MathOperation.add(1, 4));
+        System.out.println(MathOperation.add(1, 4, 6));
+        System.out.println(MathOperation.add(1, 4, 7, 8));
+
+
+
+
+
+    }
+}
