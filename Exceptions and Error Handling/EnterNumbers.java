64e4f52547174be15a10dcfe93d671924e3fce9e
Antoan Metodiev <Boksnem@abv.bg>
2024-01-31 20:39:16 +0200

    add solution

---
+++ Exceptions and Error Handling/EnterNumbers.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EnterNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> validNumbers = new ArrayList<>();
+        int validNumbersCounter = 0;
+        int previousBigNumber = 1;
+        while (true) {
+            if (validNumbersCounter == 10) {
+                break;
+            }
+
+            String line = scanner.nextLine();
+            try {
+
+                int number = Integer.parseInt(line);
+                if (number > previousBigNumber && number < 100) {
+                    validNumbersCounter++;
+                    previousBigNumber = number;
+                    validNumbers.add(number);
+                } else {
+                    System.out.printf("Your number is not in range %d - 100!\n", previousBigNumber);
+                }
+
+            } catch (NumberFormatException ex) {
+                System.out.println("Invalid Number!");
+            }
+        }
+        // Output:
+        System.out.println(String.join(", ", validNumbers.toString()
+                .replaceAll("[\\[\\]]", "")));
+    }
+}
