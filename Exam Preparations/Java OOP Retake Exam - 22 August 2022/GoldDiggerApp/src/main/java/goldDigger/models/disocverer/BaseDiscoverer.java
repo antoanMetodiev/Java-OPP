853c9b526bde40298af70bc33a5ee0180a57f6ee
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-03-12 22:12:43 +0200

    Update BaseDiscoverer.java

--- Exam Preparations/Java OOP Retake Exam - 22 August 2022/GoldDiggerApp/src/main/java/goldDigger/models/disocverer/BaseDiscoverer.java
+++ Exam Preparations/Java OOP Retake Exam - 22 August 2022/GoldDiggerApp/src/main/java/goldDigger/models/disocverer/BaseDiscoverer.java
@@ -5,7 +5,6 @@ import goldDigger.models.museum.BaseMuseum;
 import goldDigger.models.museum.Museum;
 
 public abstract class BaseDiscoverer implements Discoverer {
-
     private String name;
     private double energy;
     private Museum museum;
@@ -17,26 +16,22 @@ public abstract class BaseDiscoverer implements Discoverer {
         this.museum = new BaseMuseum();
     }
 
-
     public void setName(String name) {
         if (name == null || name.trim().isEmpty()) {
             throw new NullPointerException(ExceptionMessages.DISCOVERER_NAME_NULL_OR_EMPTY);
         }
         this.name = name;
-
     }
 
     public void setEnergy(double energy) {
         if (energy < 0) {
             throw new IllegalArgumentException(ExceptionMessages.DISCOVERER_ENERGY_LESS_THAN_ZERO);
         }
-
         this.energy = energy;
     }
 
     @Override
     public String getName() {
-
         return name;
     }
 
