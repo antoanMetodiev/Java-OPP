08c7af25acabb188aa4b0a35809d9b906e5e6ba7
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-03-12 22:14:27 +0200

    Update SpotImpl.java

--- Exam Preparations/Java OOP Retake Exam - 22 August 2022/GoldDiggerApp/src/main/java/goldDigger/models/spot/SpotImpl.java
+++ Exam Preparations/Java OOP Retake Exam - 22 August 2022/GoldDiggerApp/src/main/java/goldDigger/models/spot/SpotImpl.java
@@ -7,7 +7,7 @@ import java.util.Collection;
 
 public class SpotImpl implements Spot {
     private String name;
-    private Collection<String> exhibits; // експонати
+    private Collection<String> exhibits; 
 
     public SpotImpl(String name) {
         this.setName(name);
