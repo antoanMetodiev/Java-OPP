fff635ceb62dd2f5755febadf07db8770eef5c58
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-03-13 22:05:32 +0200

    Update BaseDelicacy.java

--- Exam Preparations/Java OOP Regular Exam - 10 December 2022/ChristmasStoreApp/src/main/java/christmasPastryShop/entities/delicacies/interfaces/BaseDelicacy.java
+++ Exam Preparations/Java OOP Regular Exam - 10 December 2022/ChristmasStoreApp/src/main/java/christmasPastryShop/entities/delicacies/interfaces/BaseDelicacy.java
@@ -15,7 +15,6 @@ public abstract class BaseDelicacy implements Delicacy {
 
     private void setName(String name) {
         if (name == null || name.trim().isEmpty()) {
-            // "Name cannot be null or white space!"
             throw new IllegalArgumentException(ExceptionMessages.INVALID_NAME);
         }
         this.name = name;
@@ -23,7 +22,6 @@ public abstract class BaseDelicacy implements Delicacy {
 
     private void setPortion(double portion) {
         if (portion <= 0) {
-            // "Portion cannot be less or equal to zero!"
             throw new IllegalArgumentException(ExceptionMessages.INVALID_PORTION);
         }
         this.portion = portion;
@@ -31,7 +29,6 @@ public abstract class BaseDelicacy implements Delicacy {
 
     private void setPrice(double price) {
         if (price <= 0) {
-            // "Price cannot be less or equal to zero!"
             throw new IllegalArgumentException(ExceptionMessages.INVALID_PRICE);
         }
         this.price = price;
@@ -54,7 +51,6 @@ public abstract class BaseDelicacy implements Delicacy {
 
     @Override
     public String toString() {
-        // "{current delicacy name}: {current portion - formatted to the second digit}g - {current price - formatted to the second digit}"
         return String.format("%s: %.2fg" +
                 " - %.2f%n", this.getName(), this.portion, this.price);
     }
