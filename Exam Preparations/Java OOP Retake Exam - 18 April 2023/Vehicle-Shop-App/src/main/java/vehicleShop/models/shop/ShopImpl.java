5cc7e002a7ef2cd6ef9cf18a770439441508ef1d
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-03-28 16:50:11 +0200

    Update ShopImpl.java

--- Exam Preparations/Java OOP Retake Exam - 18 April 2023/Vehicle-Shop-App/src/main/java/vehicleShop/models/shop/ShopImpl.java
+++ Exam Preparations/Java OOP Retake Exam - 18 April 2023/Vehicle-Shop-App/src/main/java/vehicleShop/models/shop/ShopImpl.java
@@ -8,19 +8,12 @@ public class ShopImpl implements Shop {
 
     @Override
     public void make(Vehicle vehicle, Worker worker) {
-        //  Работникът започва да произвежда превозното средство.
-        //  Това е възможно само ако работникът има сила и инструмент, който не е повреден.
-
-        // TODO:
         while (worker.canWork() && checkWorkerHaveTool(worker) && !vehicle.reached()) {
             worker.working();
             Tool tool = worker.getTools().stream().filter(e -> !e.isUnfit()).findFirst().orElse(null);
             tool.decreasesPower();
             vehicle.making();
         }
-
-        //⦁ Продължавайте да работите, докато превозното средство не
-        // бъде направено или работникът има сила (и инструменти за използване).
     }
 
     private boolean checkWorkerHaveTool(Worker worker) {
