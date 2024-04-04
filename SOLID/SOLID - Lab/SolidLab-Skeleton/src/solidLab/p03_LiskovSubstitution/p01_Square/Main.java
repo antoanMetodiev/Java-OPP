718e446cee1e2ae61fb817c1297513e20b1e15ab
Antoan Metodiev <Boksnem@abv.bg>
2024-04-04 18:55:16 +0300

    add code

---
+++ SOLID/SOLID - Lab/SolidLab-Skeleton/src/solidLab/p03_LiskovSubstitution/p01_Square/Main.java
@@ -0,0 +1,19 @@
+package solidLab.p03_LiskovSubstitution.p01_Square;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // Какво е Polymorphism - Способноста на Референцията да
+        // придобива формите на класовете който е наследяват
+
+        List<Integer> linkedList = new LinkedList<>();
+
+        System.out.println("daa".concat("nee"));
+
+
+    }
+}
