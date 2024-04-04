8fd2436d2997d6a3bb253eca7a1975460218a138
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-04-04 18:45:39 +0300

    Update EngineImpl.java

--- Exam Preparations/Java OOP Regular Exam - 16 August 2020/OnlineShop/src/onlineShop/core/EngineImpl.java
+++ Exam Preparations/Java OOP Regular Exam - 16 August 2020/OnlineShop/src/onlineShop/core/EngineImpl.java
@@ -18,7 +18,6 @@ public class EngineImpl implements Engine {
     private Controller controller;
 
     public EngineImpl() {
-        // this.controller = new ControllerImpl();   //TODO Implement first
         this.reader = new ConsoleReader();
         this.writer = new ConsoleWriter();
     }
@@ -49,7 +48,7 @@ public class EngineImpl implements Engine {
         CommandType command = CommandType.valueOf(tokens[0]);
         String[] data = Arrays.stream(tokens).skip(1).toArray(String[]::new);
 
-        /* switch (command) { // TODO Implement first
+        /* switch (command) { 
             case AddComputer:
                 return this.controller.addComputer(data[0], Integer.parseInt(data[1]), data[2], data[3], Double.parseDouble(data[4]));
             case AddPeripheral:
@@ -72,5 +71,4 @@ public class EngineImpl implements Engine {
          */
         return null;
     }
-
 }
